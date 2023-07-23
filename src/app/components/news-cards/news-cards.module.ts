34d5bbb5be3741ad3d616fa0714fcb6e4bb0bafa
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-23 23:48:18 +0530

    Add declarations,imports and exports in news-card module

--- src/app/components/news-cards/news-cards.module.ts
+++ src/app/components/news-cards/news-cards.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsCardsComponent } from './news-cards.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NewsCardsComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [NewsCardsComponent]
 })
 export class NewsCardsModule { }
