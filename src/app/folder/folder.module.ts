ed67f0d571c4f2d00efec3b1f65ed21df810c21c
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-23 23:53:35 +0530

    Add news-card module in folder module

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { NewsCardsModule } from '../components/news-cards/news-cards.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule, 
+    NewsCardsModule,
   ],
   declarations: [FolderPage]
 })
