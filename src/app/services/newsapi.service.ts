abc5ed8bb72d2939169d7f35f01bb49480743d17
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-24 12:48:04 +0530

    Update News API service to get news

--- src/app/services/newsapi.service.ts
+++ src/app/services/newsapi.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsapiService {
+  topHeadlinesUrl = environment.baseUrl;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getTopCountryHeadlines(countryCode: string, category: string) {
+    return this.http.get(this.topHeadlinesUrl+`country=${countryCode}&category=${category}&pageSize=10& apiKey=${environment.apiKey}`)
+  }
 }
