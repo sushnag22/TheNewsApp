fccbd0cd9f38b131534254ab9d8ab67a424f2d54
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-25 19:21:39 +0530

    Add news response in news service

--- src/app/services/newsapi.service.ts
+++ src/app/services/newsapi.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NewsResponse } from '../interfaces/news-response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,10 @@ export class NewsapiService {
   
   constructor(private http: HttpClient) { }
 
-  getTopCountryHeadlines(countryCode: string, category: string) {
-    return this.http.get(this.topHeadlinesUrl+`country=${countryCode}&category=${category}&pageSize=10& apiKey=${environment.apiKey}`)
+  getTopCountryHeadlines(
+    countryCode: string, 
+    category: string
+    ):Observable<NewsResponse> {
+    return this.http.get<NewsResponse>(this.topHeadlinesUrl+`country=${countryCode}&category=${category}&pageSize=10& apiKey=${environment.apiKey}`)
   }
 }
