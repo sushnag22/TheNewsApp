122dc9baadf805dc30d9e025e81bd6b11c7fb536
sushnag22 <sushruthnagaraj.github@gmail.com>
2023-07-29 22:01:56 +0530

    Update news card component module

--- src/app/components/news-cards/news-cards.module.ts
+++ src/app/components/news-cards/news-cards.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NewsCardsComponent } from './news-cards.component';
+
 import { IonicModule } from '@ionic/angular';
-
-
+import { NewsCardComponent } from './news-cards.component';
 
 @NgModule({
-  declarations: [NewsCardsComponent],
   imports: [CommonModule, IonicModule],
-  exports: [NewsCardsComponent]
+  declarations: [NewsCardComponent],
+  exports: [NewsCardComponent],
 })
-export class NewsCardsModule { }
+export class NewsCardComponentModule {}
